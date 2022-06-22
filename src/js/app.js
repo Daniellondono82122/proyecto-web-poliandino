e66792aa0c77bbffae1c3bf42e2584c120835126
Daniel Londoño <dlondono@arrivelogistics.com>
2022-06-22 17:47:03 -0500

    initial page

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+/* curator-feed-default-feed-layout */
+(function(){
+    var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
+    i.src="https://cdn.curator.io/published/b1edce84-fad7-4897-85f3-7a9f21bfca9b.js";
+    e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+    })();
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
